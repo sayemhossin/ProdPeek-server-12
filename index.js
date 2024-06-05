8ded6450bf72d3817cb100fa4db9711833d80df9
sayemhossin <sayemhossin22@gmail.com>
2024-06-06 00:12:16 +0600

    added moderator function

--- index.js
+++ index.js
@@ -111,6 +111,18 @@ async function run() {
             res.send(result)
           })
 
+        app.patch('/users/moderator/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+              $set: {
+                role: 'moderator'
+              }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+          })
+
 
 
 
